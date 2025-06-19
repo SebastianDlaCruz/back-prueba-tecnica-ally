39c12273618aac79acec89403620076b76f73c0e
SebastianDlaCruz <sebastiandelacruz335@linktic.com>
2025-06-19 12:59:16 -0300

    update

--- src/config/cookie/cookie.config.ts
+++
@@ -1,9 +0,0 @@
-import { CookieOptions } from "express";
-
-const isProduction = process.env.NODE_ENV === 'pro';
-export const cookieConfig: CookieOptions = {
-  httpOnly: true, // Impide acceso desde JavaScript
-  secure: isProduction, // Solo HTTPS en producción
-  sameSite: isProduction ? 'strict' : 'lax', // Protección CSRF
-
-};
